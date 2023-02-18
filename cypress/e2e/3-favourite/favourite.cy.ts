8e7ff93192d70d62fe8755c62bab7d85400ffab0
mitchellabrouwer <mbarron198@gmail.com>
2023-02-18 11:27:58 +1100

    test: add favourite tests

---
+++ cypress/e2e/3-favourite/favourite.cy.ts
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+describe("favourites functionality", () => {
+  it("i can add, update, remove a favourite", () => {});
+
+  it("it displays", () => {});
+});
+
+export {};
