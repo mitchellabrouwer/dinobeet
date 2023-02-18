da3ce7fc24a1442a4a6ee83df0f975cdf1654f5e
mitchellabrouwer <mbarron198@gmail.com>
2023-02-18 15:56:20 +1100

    feat: toggle dark button

---
+++ components/common/ToggleDark.tsx
@@ -0,0 +1,16 @@
+import { BsFillMoonFill, BsFillSunFill } from "react-icons/bs";
+import useToggleDark from "../../lib/hook/useToggleDark";
+
+export default function ToggleDark() {
+  const [theme, setTheme] = useToggleDark();
+
+  return (
+    <button type="button">
+      {theme === "dark" ? (
+        <BsFillMoonFill onClick={() => setTheme("light")} />
+      ) : (
+        <BsFillSunFill onClick={() => setTheme("dark")} />
+      )}
+    </button>
+  );
+}
--- lib/utils.ts
+++ lib/utils.ts
@@ -22,3 +22,15 @@ export const handleJoinClick = async () => {
     sessionId,
   });
 };
+
+export const getFromStorage = (key) => {
+  if (typeof window !== "undefined") {
+    return localStorage.getItem(key);
+  }
+};
+
+export const setToStorage = (key, value) => {
+  if (typeof window !== "undefined") {
+    return localStorage.setItem(key, value);
+  }
+};
