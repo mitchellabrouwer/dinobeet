5bbe2f11a72788a61f7ee1e63858f6affe1228b9
mitchellabrouwer <mbarron198@gmail.com>
2023-03-13 21:37:23 +1100

    feat: strikethrough on checkbox tick

--- components/recipe/Method.tsx
+++ components/recipe/Method.tsx
@@ -15,16 +15,16 @@ export const Method: FC<MethodProps> = ({ method }) => {
           {group.heading && <Heading as="h3">{group.heading}</Heading>}
           <ul>
             {group.instructions.map((instruction, x) => (
-              <li className="p-1">
+              <li className="flex p-1">
                 <input
                   id={`method ${y} ${x}`}
                   type="checkbox"
                   value={`${y} ${x}`}
-                  className="accent--500 h-4 w-4 rounded border-gray-300 bg-gray-100 text-green-600 accent-green-500 focus:ring-2 focus:ring-green-500 dark:ring-offset-gray-700 dark:focus:ring-green-600 dark:focus:ring-offset-gray-700"
+                  className="h-5 w-5 rounded border-gray-300 bg-gray-100 text-green-600 accent-green-500 focus:ring-2 focus:ring-green-500 dark:ring-offset-gray-700 dark:focus:ring-green-600 dark:focus:ring-offset-gray-700"
                 ></input>
                 <label
                   htmlFor={`method ${y} ${x}`}
-                  className="ml-2 py-3 text-sm font-medium text-gray-900"
+                  className="strikethrough ml-2 text-sm font-medium text-gray-900"
                 >
                   {instruction}
                 </label>
